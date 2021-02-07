fd9d7375e6277847beff7194171149a516233b76
student12 <zzzdanilxxx@bk.ru>
2021-02-07 12:52:07 +0300

    3 задание
    2 новых предиката

--- лабораторная работа номер 1/задание_1.pl
+++ лабораторная работа номер 1/задание_1.pl
@@ -67,4 +67,5 @@ parent(настя,вова).
 mans:-man(X),write(X),nl,fail.
 womans:-woman(X),write(X),nl,fail.
 children(X):-parent(X,Y),write(Y),nl,fail.
-
+mother(X, Y):-parent(X,Y),woman(X).
+mother(X):-parent(Y,X),woman(Y),write(Y).
