d72e63ec451863530bde1c15a510cec42d48b1dd
student12 <zzzdanilxxx@bk.ru>
2021-02-07 12:29:43 +0300

    задание номер 2
    добавление отношение родитель , создание предиката children

--- лабораторная работа номер 1/задание_1.pl
+++ лабораторная работа номер 1/задание_1.pl
@@ -25,10 +25,46 @@ woman(жанна).
 woman(настя).
 
 parent(артем,рита).
-parent(артем,рита).
-parent(артем,рита).
+parent(артем,павел).
+parent(артем,акакий).
+
+parent(мария,рита).
+parent(мария,павел).
+parent(мария,акакий).
+
+parent(михаил,гарри).
+parent(михаил,настя).
+parent(ихаил,алексей).
+
+parent(алина,гарри).
+parent(алина,настя).
+parent(алина,алексей).
+
+parent(нина,полина).
+parent(нина,рикки).
+parent(виктор,полина).
+parent(виктор,рикки).
+
+parent(рита,ваня).
+parent(рита,никита).
+parent(гарри,ваня).
+parent(гарри,никита).
+
+parent(алексей,роджер).
+parent(полина,роджер).
+
+parent(акакий,жанна).
+parent(акакий,оля).
+parent(акакий,вова).
+
+parent(настя,жанна).
+parent(настя,оля).
+parent(настя,вова).
+
+
 
 
 mans:-man(X),write(X),nl,fail.
 womans:-woman(X),write(X),nl,fail.
+children(X):-parent(X,Y),write(Y),nl,fail.
 
