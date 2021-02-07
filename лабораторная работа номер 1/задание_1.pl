42441ed48cf9fa3aa1ec10cda6f27294cca8d88e
student12 <zzzdanilxxx@bk.ru>
2021-02-07 16:56:00 +0300

    Задание 5 вариант 12
    2 предиката brother

--- лабораторная работа номер 1/задание_1.pl
+++ лабораторная работа номер 1/задание_1.pl
@@ -71,3 +71,5 @@ mother(X, Y):-parent(X,Y),woman(X).
 mother(X):-parent(Y,X),woman(Y),write(Y).
 douther(X,Y):-parent(Y,X),woman(X).
 douther(X):-mother(X,Y),woman(Y),write(Y),nl,fail.
+brother(X,Y):-man(X),parent(Z,X),parent(Z,Y).
+brother(X):-parent(Y,X),parent(Y,Z),man(Z),dif(X,Z),write(Z),nl,fail.
