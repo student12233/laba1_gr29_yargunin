5ceaf63050a78e1d2b46362efdb4a04b51aff671
student12 <zzzdanilxxx@bk.ru>
2021-02-07 20:34:11 +0300

    задание 12 вариант 12
    предикат дядя

--- лабораторная работа номер 1/задание_1.pl
+++ лабораторная работа номер 1/задание_1.pl
@@ -71,7 +71,7 @@ mother(X,Y):-parent(X,Y),woman(X).
 mother(X):-parent(Y,X),woman(Y),write(Y).
 douther(X,Y):-parent(Y,X),woman(X).
 douther(X):-mother(X,Y),woman(Y),write(Y),nl,fail.
-brother(X,Y):-man(X),parent(Z,X),parent(Z,Y).
+brother(X,Y):-man(X),parent(Z,X),parent(Z,Y),dif(X,Y).
 brother(X):-parent(Y,X),parent(Y,Z),man(Z),dif(X,Z),write(Z),nl,fail.
 husband(X,Y):-parent(X,Z),parent(Y,Z),man(X).
 husband(X):-parent(X,Y),parent(Z,Y),man(Z),write(Z).
@@ -83,3 +83,5 @@ grand_da(X, Y):-woman(X),parent(Z,X),parent(Y,Z).
 grand_dats(X):-parent(X,Y),parent(Y,Z),woman(Z),write(Z),nl,fail.
 grand_pa_and_son(X,Y):-grand_pa(X,Y),man(Y);grand_pa(Y,X),man(X).
 grand_pa_and_da(X,Y):-grand_da(X,Y),woman(Y);grand_da(Y,X),woman(X).
+dada(X,Y):-brother(X,Z),parent(Z,Y).
+dada(X):-parent(Y,X),brother(Z,Y),write(Z),nl,fail.
