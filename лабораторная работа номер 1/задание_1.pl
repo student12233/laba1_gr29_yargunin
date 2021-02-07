2e4783028f0eb358933577913eb7b6c3c0d1604e
student12 <zzzdanilxxx@bk.ru>
2021-02-07 20:51:44 +0300

    13 задание вариант 12
    предикат племяницы

--- лабораторная работа номер 1/задание_1.pl
+++ лабораторная работа номер 1/задание_1.pl
@@ -75,7 +75,7 @@ brother(X,Y):-man(X),parent(Z,X),parent(Z,Y),dif(X,Y).
 brother(X):-parent(Y,X),parent(Y,Z),man(Z),dif(X,Z),write(Z),nl,fail.
 husband(X,Y):-parent(X,Z),parent(Y,Z),man(X).
 husband(X):-parent(X,Y),parent(Z,Y),man(Z),write(Z).
-b_s(X,Y):-parent(Z,X),parent(Z,Y).
+b_s(X,Y):-parent(Z,X),parent(Z,Y),dif(X,Y).
 b_s(X):-parent(Z,X),woman(Z),parent(Z,Y),dif(X,Y),write(Y),nl,fail.
 grand_pa(X,Y):-parent(Z,Y),parent(X,Z),man(X).
 grand_pas(X):-parent(Z,X),parent(Y,Z),man(Y),write(Y),nl,fail.
@@ -85,3 +85,7 @@ grand_pa_and_son(X,Y):-grand_pa(X,Y),man(Y);grand_pa(Y,X),man(X).
 grand_pa_and_da(X,Y):-grand_da(X,Y),woman(Y);grand_da(Y,X),woman(X).
 dada(X,Y):-brother(X,Z),parent(Z,Y).
 dada(X):-parent(Y,X),brother(Z,Y),write(Z),nl,fail.
+sister(X,Y):-woman(X),parent(Z,X),parent(Z,Y),dif(X,Y).
+plem(X,Y):-woman(X),parent(Z,X),b_s(Y,Z).
+plem(X):-plem(Y,X),write(Y),nl,fail.
+
