103c1ea83b119d465ab11c95f5138cbbd286ebcb
student12 <zzzdanilxxx@bk.ru>
2021-02-19 21:53:56 +0300

    Задание 4

--- лабораторная работа номер 3/lab_3.pl
+++ лабораторная работа номер 3/lab_3.pl
@@ -1,6 +1,6 @@
 max(X,Y,X):-X>Y,!.
 max(_,Y,Y):-!.
 max(X,Y,Z,U):-max(X,Y,T),max(T,Z,U).
-fact(1,1):-!.
-fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
-
+fact(N,X):-factorial(N,X,1).
+factorial(1,X,X):-!.
+factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
