286cd115caaeeaa9e4273a0c73d3e15fb3e6c92d
student12 <zzzdanilxxx@bk.ru>
2021-02-24 15:20:20 +0300

    3 лаба задача 6

--- лабораторная работа номер 3/lab_3.pl
+++ лабораторная работа номер 3/lab_3.pl
@@ -4,7 +4,10 @@ max(X,Y,Z,U):-max(X,Y,T),max(T,Z,U).
 fact(N,X):-factorial(N,X,1).
 factorial(1,X,X):-!.
 factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
-fib(1,1):-!.
-fib(2,1):-!.
-fib(N,X):-N1 is N-1,N2 is N-2,fib(N1,X1),fib(N2,X2),X is X1+X2 .
+fib(N,X):-fibo(N,1,1,0,X).
+fibo(N,N,Xt,Xtp,Xt):-!.
+fibo(N,Nt,Xt,Xtp,X):-Nt1 is Nt+1,Xt1 is Xt + Xtp,fibo(N,Nt1,Xt1,Xt,X).
+
+
+
 
