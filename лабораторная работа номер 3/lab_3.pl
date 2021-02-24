35ddb56c3f9b2580b9e73ec7a06341cb24b5b375
student12 <zzzdanilxxx@bk.ru>
2021-02-24 18:26:16 +0300

    задание 11 рекурсия вверх лаба 3

--- лабораторная работа номер 3/lab_3.pl
+++ лабораторная работа номер 3/lab_3.pl
@@ -14,3 +14,7 @@ min(X,M):-M1 is X mod 10,X1 is X div 10,min(X1,M,M1).
 min(X,M,Mt):- X1 is X div 10, X1 is 0,M is Mt,!.
 min(X,M,Mt):-((Mt>X mod 10,M1 is X mod 10,!);(M1 is Mt,!)),X1 is X div 10,!,min(X1,M,M1).
 
+m_3(X,M):- X1 is X div 10, X1 is 0,M1 is X mod 10,((M1<3,M is 1);M is 0),!.
+m_3(X,M):-X1 is X div 10,m_3(X1,M1),((3 < X mod 10,M is M1,!);(M is M1+1,!)).
+
+
