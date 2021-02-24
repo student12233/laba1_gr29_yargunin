61f9f83a21dad9d43b90539c37cdbad4c4ace3a4
student12 <zzzdanilxxx@bk.ru>
2021-02-24 15:47:02 +0300

    лаба 3 задание 8

--- лабораторная работа номер 3/lab_3.pl
+++ лабораторная работа номер 3/lab_3.pl
@@ -7,8 +7,9 @@ factorial(N,X,X1):-N1 is N-1,X2 is X1*N,factorial(N1,X,X2).
 fib(N,X):-fibo(N,1,1,0,X).
 fibo(N,N,Xt,_,Xt):-!.
 fibo(N,Nt,Xt,Xtp,X):-Nt1 is Nt+1,Xt1 is Xt + Xtp,fibo(N,Nt1,Xt1,Xt,X).
-sum(X,S):- X1 is X div 10 ,X1 is 0,S=X.
-sum(X,S):-X1 is X div 10,sum(X1,S1),S is S1+X mod 10.
+sum(X,S):-sum(X,S,0).
+sum(0,St,St):-!.
+sum(X,S,St):-St1 is (X mod 10)+St,X1 is X div 10,sum(X1,S,St1).
 
 
 
