ce1c0b20ded0eaf5972c635c6621749f456c75d5
student12 <zzzdanilxxx@bk.ru>
2021-03-09 19:09:31 +0300

    лаба 4 задание 2

--- лабораторная работа номер 4/lab_4.pl
+++ лабораторная работа номер 4/lab_4.pl
@@ -6,3 +6,8 @@ read_list(N,X):-read(X1),N1 is N-1,read_list(N1,X2),screp([X1],X2,X).
 write_list([X]):-write(X).
 write_list([X|Y]):-write(X),write_list(Y).
 read_and_write(N):-read_list(N,X),write_list(X).
+sum_list_down(List, Summ):-sum(List,0,Summ).
+sum([],S,S):-!.
+sum([H|Ti],T,S):-T1 is T+H,sum(Ti,T1,S).
+suum_list_down(N):-read_list(N,X),sum_list_down(X,S),write(S).
+
