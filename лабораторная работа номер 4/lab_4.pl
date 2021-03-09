7ffb3c78509b70b4ceb5be97630075cab8072c7a
student12 <zzzdanilxxx@bk.ru>
2021-03-09 19:34:45 +0300

    задание 4.1 лаба 4

--- лабораторная работа номер 4/lab_4.pl
+++ лабораторная работа номер 4/lab_4.pl
@@ -13,3 +13,9 @@ suum_list_down(N):-read_list(N,X),sum_list_down(X,S),write(S).
 sum_list_up([H|T], Summ):-sum_list_up(T, Summ1),Summ is Summ1+H.
 sum_list_up([], 0):-!.
 
+list_el_numb([], _, _):-!.
+list_el_numb([H|_],H,1):-!.
+list_el_numb(List, Elem, Number):-list_el(List,Elem,Number,1).
+list_el([],_,_,_):-fail,!.
+list_el([H|_],Elem,Tec_num,Tec_num):-Elem is H,!.
+list_el([_|T],Elem,Num,Tec):-Tec1 is Tec+1,list_el(T,Elem,Num,Tec1).
