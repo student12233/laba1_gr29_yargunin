ee8ff121b550e4f7f9eb878e3daae44ba63e897d
student12 <zzzdanilxxx@bk.ru>
2021-05-16 22:02:38 +0300

    лаба 7 задание 2

--- лабораторная работа номер 7/lab7.pl
+++ лабораторная работа номер 7/lab7.pl
@@ -11,3 +11,31 @@ r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 p1:- read_str(List,Length), write_str(List), write(", "), write_str(List),
 	write(", "), write_str(List), nl, write("Length = "), write(Length).
 
+pr5_2:-read_str(A,_),count_words(A,K),write(K).
+
+count_words(A,K):-count_words(A,0,K).
+
+count_words([],K,K):-!.
+count_words(A,I,K):-skip_space(A,A1),get_word(A1,Word,A2),Word \=[],I1 is I+1,count_words(A2,I1,K),!.
+count_words(_,K,K).
+
+skip_space([32|T],A1):-skip_space(T,A1),!.
+skip_space(A1,A1).
+
+get_word([],[],[]):-!.
+get_word(A,Word,A2):-get_word(A,[],Word,A2).
+
+get_word([],Word,Word,[]).
+get_word([32|T],Word,Word,T):-!.
+get_word([H|T],W,Word,A2):-append(W,[H],W1),
+							get_word(T,W1,Word,A2).
+
+
+
+get_words(A,Words,K):-get_words(A,[],Words,0,K).
+
+get_words([],B,B,K,K):-!.
+get_words(A,Temp_words,B,I,K):-
+	skip_space(A,A1),get_word(A1,Word,A2),Word \=[],
+	I1 is I+1,append(Temp_words,[Word],T_w),get_words(A2,T_w,B,I1,K),!.
+get_words(_,B,B,K,K).
