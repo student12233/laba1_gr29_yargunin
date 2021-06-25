ad3097ff4bd1f858ff1de4049f10225f61bab795
student12 <zzzdanilxxx@bk.ru>
2021-06-25 13:24:05 +0300

    задание 5-7 лаба 19
    создание запросов

--- лабораторная работа номер 19/src/StartProgram.kt
+++ лабораторная работа номер 19/src/StartProgram.kt
@@ -50,4 +50,24 @@ fun main()
     val jurnall = readFromJsonFile<journal>("json/journal.json")
 
 
+    val prim1=jurnall[0].spisok.groupBy {it.stud}.filterNot {jurnall[0].spisok.filter{ it.end_ochenka == 3 || it.end_ochenka == 2 }.groupBy { it.stud }.contains(it.key)}.keys.filter { !it.budget }.filter { it.motherLand!="Россия" }.toString()
+    val prim2 = ((jurnall[0].spisok.filter { it.stud.group ==29 }.filter { it.predmet == "дискретная математика"}.sumOf { it.end_ochenka }.toDouble())  /jurnall[0].spisok.filter { it.stud.group ==29 }.filter { it.predmet == "дискретная математика"}.count())
+    val prim3 = jurnall[0].spisok.groupBy { it.end_ochenka }.filter { it.key==2 }.values.flatMap{it}.distinctBy { it.stud.name +it.stud.sename}.filter { it.poitki ==3 }.groupBy { it.stud }.keys.toString()
+    val prim4 =jurnall[0].spisok.filter { groups_st.first{ it.teacher?.name=="Александр" &&it.teacher?.sename=="Климец" }.numberGroup==it.stud.group }.filter { it.end_ochenka==5&& it.predmet=="дискретная математика" }.groupBy { it.stud }.keys
+    val prim5 = jurnall[0].spisok.filter { it.stud.budget==false&&it.stud.motherLand!="Россия" }.groupBy { it.stud }.maxByOrNull { it.value.sumOf { it.end_ochenka } }?.key.toString()
+
+    print("иностранные студенты у которых есть шанс попасть на бюджет : ")
+    println(prim1)
+    print("средняя оценка 29 группы по дискретной математике :")
+    println(prim2)
+    print("студенты которые могут быть отчисленны : ")
+    println(prim3)
+    print("студенты из группы которую курирует Александр Климец и у которых по дискретной математике оценка 5 : ")
+    println(prim4)
+    print("иностранный студент на комерции у которого наибольший средний балл: ")
+    println(prim5)
+
+
+
+
 }
