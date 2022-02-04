01d6a1dced8b21f7ae4fec88611ebc1abbb66259
student12 <zzzdanilxxx@bk.ru>
2022-02-04 14:34:35 +0300

    лаба 1 задание 1 конструкция case

--- ruby/лаба1/lab1_1.rb
+++ ruby/лаба1/lab1_1.rb
@@ -1,30 +1,25 @@
-
 #задание1
-=begin
-	
+=begin	
 это многострочный коментарий
-	
 =end
-
 # а это однострочный коментарий
-
-
  puts ('#а это уже не коментарий')
  puts "hello world1"
  print "hello world2\n"
  #задание 2
-
- 
  # выводим приветствие
  puts "добрый день #{ARGV[0]}"
 # получаем методы класса String
  puts String.instance_methods
-
-
 #задание 3
 puts "какой язык програмированния вам больше нравится"
 lang = STDIN.gets.chomp
-x = if lang =="ruby" then puts "подлиза" else puts "а будет руби" end
+case "ruby"
+when STDIN.gets.chomp
+	puts "подлиза"
+else
+	puts "а будет руби"
+end
 # задание 4
 puts "введите команду руби"
 rub=STDIN.gets.chomp
