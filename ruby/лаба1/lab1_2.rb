dff1c2198b344292694c2ab25f971c509fd88f05
student12 <zzzdanilxxx@bk.ru>
2022-02-04 21:17:42 +0300

    лаба 1 задание 2 подзадание 3

--- ruby/лаба1/lab1_2.rb
+++ ruby/лаба1/lab1_2.rb
@@ -3,17 +3,37 @@
 def sum_chi(chi)
 	return chislo=chi.to_s.chars.inject(0){|sum,x| sum + x.to_i }
 end
-
-
 def max_chi(chi)
 	return chislo=chi.to_s.chars.max
 end
-
 def min_chi(chi)
 	return chislo=chi.to_s.chars.min
 end
-
 def mult_chi(chi)
 	return chislo=chi.to_s.chars.inject(1){|sum,x| sum * x.to_i }
 end
+def prost(chi)
+return x=(1..(chi/2)+1).select{|x| chi%x==0}.size<=1
+end
+def method_1(chi)
+ return (1..(chi/2)+1).select{|x| prost(x) and chi%x==0}.max
+end
+def method_2(chi)
+	return chislo=chi.to_s.chars.select{|x| x!=5 }.inject(1){|sum,x| sum * x.to_i }
+end
+def Nod(chi1,chi2)
+ return chi1 if chi1==chi2
+ return Nod([chi1,chi2].min,(chi1-chi2).abs)
+end
+def method_3(chi)
+ return Nod(mult_chi(chi),(1..(chi)).select{|x| (not prost(x)) and chi%x==0 and x%2!=0}.max)
+ end
 
+if ARGV.size==0
+	print("Hello world")
+
+else 
+	print("введите число")
+	arg=STDIN.gets.chomp
+	puts eval(ARGV[0]+"("+arg+")")
+end
