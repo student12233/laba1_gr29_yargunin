9c9f73d2ffc41a0c87799a8c5167b33044f61f2e
student12 <zzzdanilxxx@bk.ru>
2022-02-04 18:04:46 +0300

    лаба 1 задание 2 подзадание 2

--- ruby/лаба1/lab1_2.rb
+++ ruby/лаба1/lab1_2.rb
@@ -1,2 +1,19 @@
-puts chislo=ARGV[0].chars.inject(0){|sum,x| sum + x.to_i }
+
+
+def sum_chi(chi)
+	return chislo=chi.to_s.chars.inject(0){|sum,x| sum + x.to_i }
+end
+
+
+def max_chi(chi)
+	return chislo=chi.to_s.chars.max
+end
+
+def min_chi(chi)
+	return chislo=chi.to_s.chars.min
+end
+
+def mult_chi(chi)
+	return chislo=chi.to_s.chars.inject(1){|sum,x| sum * x.to_i }
+end
 
