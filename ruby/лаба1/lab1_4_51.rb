8c15a6839d6f88367848d169200c4a48a2a33143
student12 <zzzdanilxxx@bk.ru>
2022-02-05 15:15:54 +0300

    лаба1 задание 4 подзадание 51

--- ruby/лаба1/lab1_4_51.rb
+++ ruby/лаба1/lab1_4_51.rb
@@ -1,7 +1,14 @@
-
-def method_27(mass)
-return mass[1,mass.size-1]+[mass[0]]
+def method_51_L1(mass)
+ return  mass.uniq
 end
+
+def method_51_L2(mass)
+	mass_un = method_51_L1(mass)
+return mass_un.map{|x| x=mass.select{|y| y==x}.size}
+end
+
+
+
 if ARGV[0]=="клава" then
 	puts "введите массив"
 	string=(STDIN.gets.chomp).split(",").map!{|x| x.to_i}
@@ -9,5 +16,7 @@ else
 	string = File.open(ARGV[1],"r"){ |file| file.read }
 string=string.split(",").map!{|x| x.to_i}
 end
-puts method_27(string)
+puts method_51_L1(string)
+puts method_51_L2(string)
+
 
