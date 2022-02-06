b0acd999a9e7163bca14edeed285526366d60441
student12 <zzzdanilxxx@bk.ru>
2022-02-06 18:28:25 +0300

    лаб1задание6подзадание2

--- ruby/лаба1/lab1_6.rb
+++ ruby/лаба1/lab1_6.rb
@@ -3,8 +3,11 @@
 def method_len(mass)
 return mass.sort{|x,y|x.size<=>y.size}
 end
+def method_kolwo_slow(mass)
+return mass.sort{|x,y| x.scan(/[a-zA-ZА-Яа-я]/).size<=>y.scan(/[a-z]|[A-Z]|[А-Я]|[а-я]/).size}
+end
 
 puts "введите номер задачи"
 number=STDIN.gets.chomp
-args = File.open("input.txt","r"){ |file| file.read }.split
+args = File.open("input.txt","r"){ |file| file.read }.split("\n")
 	puts eval ("method_#{number}(#{args})")
