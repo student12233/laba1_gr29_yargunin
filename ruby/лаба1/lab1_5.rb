ec83cf83c7be69b7a1cbee23199d79c960dcce90
student12 <zzzdanilxxx@bk.ru>
2022-02-06 14:17:27 +0300

    лаба1задание5подзадание16

--- ruby/лаба1/lab1_5.rb
+++ ruby/лаба1/lab1_5.rb
@@ -4,5 +4,6 @@ end
 def method_8(str)
 	return str.split.select{|x| x.size%2==0}.size
 end
-
-
+def method_16(mass)
+return  mass.sort{|a,b| ["белый","синий","красный"].index(a)<=>["белый","красный","синий"].index(b)}
+end
