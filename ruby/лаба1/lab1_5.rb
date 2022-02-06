8b50c3f5843b04dfedc51e9c01abd1fd11edd569
student12 <zzzdanilxxx@bk.ru>
2022-02-06 13:23:32 +0300

    лаба1 задание 5 подзадание 8

--- ruby/лаба1/lab1_5.rb
+++ ruby/лаба1/lab1_5.rb
@@ -1,5 +1,8 @@
 def method_3(str)
 return	str.split.shuffle.inject(""){|sum,x|sum+=(x+" ")}.strip!
 end
+def method_8(str)
+	return str.split.select{|x| x.size%2==0}.size
+end
 
-print method_3 "1 2 3"
+
