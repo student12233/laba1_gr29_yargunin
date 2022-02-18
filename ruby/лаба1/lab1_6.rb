bd0ea3b59c4d197e91ca676b52ca5f9ac5fecd2f
student12 <zzzdanilxxx@bk.ru>
2022-02-18 16:42:41 +0300

    лаба 1 задание 6

--- ruby/лаба1/lab1_6.rb
+++ ruby/лаба1/lab1_6.rb
@@ -1,11 +1,26 @@
 
-
+# chastota=File.open("chastota.txt","r"){ |file| file.read }.split("\n")
+	# chas_up=Array.new(chastota.size) { |i|  chastota[i].split }
 def method_len(mass)
 return mass.sort{|x,y|x.size<=>y.size}
 end
 def method_kolwo_slow(mass)
 return mass.sort{|x,y| x.scan(/[a-zA-ZА-Яа-я]/).size<=>y.scan(/[a-z]|[A-Z]|[А-Я]|[а-я]/).size}
 end
+
+def method_7(mass)
+	 mass.sort_by{|x| (x.scan(/[йцкнгшщзхъфвпрлджчсмтьб]{1}[ёеыаоэяию]{1}/).size-x.scan(/[ёеыаоэяию][йцкнгшщзхъфвпрлджчсмтьб]{1}{1}/).size)**2}
+end
+def method_12(mass)
+mass.sort_by{|x_sort|(((mass.inject(""){|sum,x|sum+=(x+" ")}.max_by{|x_max| mass.inject(""){|sum1,x1|sum1+=(x1+" ")}.scan(x_max).size})/(mass.inject(""){|sum,x|sum+=(x+" ")}.size))-((x_sort.scan(mass.inject(""){|sum,x|sum+=(x+" ")}.max_by{|x_max| mass.inject(""){|sum1,x1|sum1+=(x1+" ")}.scan(x_max)).size)/(x_sort.size)))**2}
+end
+def method_5(mass)
+mass.sort_by{|x_sort| ( ((x_sort.max_by{|x_max| x_sort.scan(x_max).size })/(x_sort.size))-((x_sort.max_by{|x_max| x_sort.scan(x_max).size )/(mass.inject(""){|sum,x|sum+=(x+" ")}.size))) }
+end
+def method_3(mass)
+mass.sort_by{|x_sort| ( ((x_sort.max_by{|x_max| x_sort.scan(x_max).size })/(x_sort.size))-((x_sort.max_by{|x_max| x_sort.scan(x_max).size )/(mass.inject(""){|sum,x|sum+=(x+" ")}.size))) }
+end
+
 
 puts "введите номер задачи"
 number=STDIN.gets.chomp
