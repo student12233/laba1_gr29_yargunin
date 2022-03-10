c6f4d6884851d7e4d2fb3611042fe078507c76ef
student12 <zzzdanilxxx@bk.ru>
2022-03-10 17:01:33 +0300

    2_1_4

--- ruby/лаба 2/lab2_1.rb
+++ ruby/лаба 2/lab2_1.rb
@@ -6,14 +6,14 @@ class Department
         @duties=[]
         @duties_index_now=0
     end
-    def ?duties_empty()
+    def duties_empty?()
       @duties.size==0
     end
     def to_s()
       return "название:#{self.name}  номер телефона:#{self.number}  обязанности:#{self.duties_read}"
     end
     def duties_add(dutie)
-      @duties.push(dutie).uniq()!
+      @duties.push(dutie).uniq!()
     end
     def duties_read()
       sum=","
@@ -27,18 +27,18 @@ class Department
       return sum
     end
     def duties_cursor(dutie)
-      @duties_index_now = @duties.find_index(dutie) if @duties.find_index(dutie)!= null
+      @duties_index_now = @duties.find_index(dutie) if @duties.find_index(dutie)!= nil
     end
     def duties_cursor_read()
-      return @duties[duties_index_now] if  !(self.?duties_empty)
+      return @duties[duties_index_now].to_s if  !(self.duties_empty?) 
     end
     def duties_cursor_update(dutie)
-      @duties[@duties_index_now]=dutie if !dutie in @duties &&  !(self.?duties_empty)
+      @duties[@duties_index_now]= dutie if !((@duties.include?(dutie)) & (self.duties_empty?))
     end
     def duties_cursor_delete()
-      @duties.delete_at(@duties_index_now) if !(self.?duties_empty)
+      @duties.delete_at(@duties_index_now) if !(self.duties_empty?) 
+      @duties_index_now=0
     end
-    
 end
 
 
@@ -49,3 +49,9 @@ a.duties_add("заняться бизнесом")
 a.duties_add("отдыхать")
 a.duties_add("z z z z")
 puts(a.duties_read)
+a.duties_cursor("отдыхать")
+a.duties_cursor_update("пом пом")
+puts(a.duties_read)
+a.duties_cursor_delete
+puts(a.duties_read)
+
